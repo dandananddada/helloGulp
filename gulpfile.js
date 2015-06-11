9f63995af573aa1e23566bcdcd0648b56d886486
Eten Gstar <dandananddada@163.com>
2015-06-11 18:32:20 +0800

    add jade and coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,37 @@
  
 // Load plugins
 var gulp = require('gulp'),
+    jade = require('gulp-jade'),
     sass = require('gulp-ruby-sass'),
+    coffee = require('gulp-coffee'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
+    gutil = require('gulp-util'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
     webserver = require('gulp-webserver'),
-    del = require('del');
+    del = require('del'),
+    coffeeStream = coffee({bare: true});
+
+    coffeeStream.on('error', gutil.log);
 
 //html
 gulp.task('html', function() {
-    return gulp.src("app/*.html")
-        .pipe(gulp.dest('dist/'))
+    return gulp.src("src/*.jade")
+        .pipe(jade({ pretty: true}))
+        .pipe(gulp.dest('app/'))
         .pipe(livereload())
         .pipe(notify({ message: 'html task complete' }));
 }); 
 
 // css
 gulp.task('css', function() {
-  return sass('app/css/', { style: 'expanded' })
+  return sass('src/styles/', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('app/css'))
     .pipe(rename({ suffix: '.min' }))
@@ -36,9 +43,10 @@ gulp.task('css', function() {
  
 // js
 gulp.task('js', function() {
-  return gulp.src('app/js/**/*.js')
+  return gulp.src('src/scripts/**/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
     //.pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('app/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
@@ -48,8 +56,9 @@ gulp.task('js', function() {
  
 // Images
 gulp.task('images', function() {
-  return gulp.src('app/images/**/*')
+  return gulp.src('src/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('app/images'))
     .pipe(gulp.dest('dist/images'))
     .pipe(livereload())
     .pipe(notify({ message: 'Images task complete' }));
@@ -57,7 +66,7 @@ gulp.task('images', function() {
 
 // Clean
 gulp.task('clean', function(cb) {
-    del(['dist/','app/css/**/*.css'], cb)
+    del(['dist/','app/'], cb)
 });
  
  
@@ -65,22 +74,22 @@ gulp.task('clean', function(cb) {
 gulp.task('watch', function() {
 
   //Watch .html files
-  gulp.watch('app/**/*.html', ['html']);
+  gulp.watch('src/**/*.jade', ['html']);
 
   // Watch .scss files
-  gulp.watch('app/css/**/*.scss', ['css']);
+  gulp.watch('src/styles/**/*.scss', ['css']);
  
   // Watch .js files
-  gulp.watch('app/js/**/*.js', ['js']);
+  gulp.watch('src/scripts/**/*.js', ['js']);
  
   // Watch image files
-  gulp.watch('app/images/**/*', ['images']);
+  gulp.watch('src/images/**/*', ['images']);
  
   // Create LiveReload server
   livereload.listen();
  
   // Watch any files in dist/, reload on change
-  gulp.watch(['dist/**']).on('change', livereload.changed);
+  gulp.watch(['app/**']).on('change', livereload.changed);
  
 });
 
